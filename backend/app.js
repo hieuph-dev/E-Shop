b4223c5470562b6bcfed18ce14ac11d477392ce5
Hieu Pham <hieupham250201@gmail.com>
2023-07-28 19:00:50 +0700

    fix cors at the end day

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,16 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-// const path = require("path");
 
 app.use(
   cors({
-    origin: ["http://shopo-byhieupham-cefl.vercel.app"],
+    origin: ["http://shopo-byhieupham-cefl.vercel.app",],
     credentials: true,
   })
 );
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use("/", express.static(path.join(__dirname,"./uploads")));
 app.use("/test", (req,res) => {
   res.send("Hello world!");
 });
