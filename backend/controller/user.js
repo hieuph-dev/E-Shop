4066e84dbd15af1a723e8582b1a2ce467ff2d0f8
Hieu Pham <hieupham250201@gmail.com>
2023-07-28 09:46:38 +0700

    cor fixed

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -39,7 +39,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `http://shopo-byhieupham-cefl.vercel.app/${activationToken}`;
 
     try {
       await sendMail({
