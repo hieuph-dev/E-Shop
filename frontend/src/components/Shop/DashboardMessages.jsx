d7a25ede51f3f36d6e8dfe7a01a92646be38f354
Hieu Pham <hieupham250201@gmail.com>
2023-07-28 10:32:32 +0700

    socket added done

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import styles from "../../styles/style";
 import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const ENDPOINT = "http://localhost:4000/";
+const ENDPOINT = "https://socket-server-ecommerce.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/style";
-const ENDPOINT = "https://socket-ecommerce-tu68.onrender.com/";
+const ENDPOINT = "https://socket-server-ecommerce.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
